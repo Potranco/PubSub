a0399d856b410e41329873d96cafe0c00b45d6bc
Chema <chemar@gmail.com>
2018-08-02 11:57:12 +0200

    refactor & create test

---
+++ test/index.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect
+var PubSub = require('../src/index.js')
+
+var events = new PubSub()
+var eventHandler = function (callback) {
+  callback()
+}
+var obj = {
+  num: 0
+}
+
+describe('Pubsub', function () {
+  it('Loaded', function () {
+    expect(typeof PubSub).to.not.equal('undefined')
+  })
+  it('Create event', function () {
+    events.on('Custom', eventHandler)
+    expect(typeof events.callbacks['Custom']).to.not.equal('undefined')
+  })
+  it('Execute event', function (done) {
+    events.emit('Custom', done)
+  })
+  it('Execute event not params', function (done) {
+    events.on('Custom2', function () { obj.num = 1})
+    events.emit('Custom2')
+    setTimeout(function () {
+      return expect(obj.num).to.equal(1) && done()
+    }, 1)
+  })
+})
